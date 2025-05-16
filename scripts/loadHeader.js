971434d98ad540aafac1a03b845f142f41f38d14
Niels <s203980@dtu.dk>
2025-05-16 11:58:22 +0200

    updated javescript

--- scripts/loadHeader.js
+++ scripts/loadHeader.js
@@ -1,6 +1,13 @@
-
 fetch('/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-container').innerHTML = data;
+
+    // After header is loaded, bind the email copy button
+    const email = "nielspeterkc@gmail.com";
+    document.getElementById("copy-email").addEventListener("click", () => {
+      navigator.clipboard.writeText(email).then(() => {
+        alert("Email address copied to clipboard!");
+      });
+    });
   });
